a009f130966b24de86b709d96d5417cba3d87e13
xDwill21 <Darrion270@gmail.com>
2022-09-16 14:16:04 -0400

    removed pass

--- cypher.py
+++ cypher.py
@@ -2,7 +2,7 @@
 # CSCI-121 Elements of Computer Programming II
 # Recitation 1 - Encryption with a password
 # *******************************************************
-
+import math
 import string
 
 alphabet = string.printable
@@ -11,9 +11,30 @@ ordinal_value = {ch: i for i, ch in enumerate(alphabet)}
 
 def encrypt(message, password):
     encrypted_message = ''
-    pass  # TODO replace this line with your code.
+    password = math.ceil(len(message)/len(password))*password
+    for index, ch in enumerate (message):
+        pass_ch = password[index]
+        key = ordinal_value[pass_ch]
+        ord_of_ch = ordinal_value[ch]
+        shifted_ord_of_ch =(ord_of_ch + key) % len(alphabet)
+        encrypted_ch = alphabet[shifted_ord_of_ch]
+        encrypted_message += encrypted_ch
+    return encrypted_message
+
 
 
 def decrypt(message, password):
     decrypted_message = ''
-    pass  # TODO replace this line with your code.
+    password = math.ceil(len(message) / len(password)) * password
+    password += password[0:len(message) % len(password)]
+    for index, ch in enumerate(message):
+        pass_ch = password[index]
+        key = ordinal_value[pass_ch]
+        ord_of_ch = ordinal_value[ch]
+        shifted_ord_of_ch = (ord_of_ch - key) % len(alphabet)
+        decrypted_ch = alphabet[shifted_ord_of_ch]
+        decrypted_message += decrypted_ch
+    return decrypted_message
+
+#if __name__== "__main__":
+   # print(decrypt("abcdefg", "bcde"))
